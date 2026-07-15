aa12d171610aa1f472814e33a04746ac1bd39eaa
Shreyash Mahalle <shreyashmahallle75@gmail.com>
2026-07-15 12:51:21 +0530

    feat(worker): implement telemetry worker thread module

--- backend/src/services/telemetry.service.ts
+++ backend/src/services/telemetry.service.ts
@@ -1,8 +1,10 @@
 import { TelemetryData } from "../interfaces/telemetry.interface";
-
 import { ApiResponse } from "../interfaces/apiResponse.interface";
+import { runTelemetryWorker } from "../workers";
 
-export const processTelemetry = (data: TelemetryData): ApiResponse => {
+export const processTelemetry = async (
+  data: TelemetryData
+): Promise<ApiResponse> => {
   const { vehicleId, latitude, longitude, speed } = data;
 
   if (
@@ -42,8 +44,26 @@ export const processTelemetry = (data: TelemetryData): ApiResponse => {
     };
   }
 
-  // TODO (Developer 2):
-  // Send telemetry to Worker Thread
+  const workerResult = await runTelemetryWorker({
+    vehicleId,
+    latitude,
+    longitude,
+    speed,
+  });
+  console.log("========== SERVICE ==========");
+console.log(workerResult);
+console.log("=============================");
+
+  if (!workerResult.success) {
+    return {
+      success: false,
+      statusCode: 400,
+      message:
+        workerResult.error ||
+        workerResult.validation?.errors.join(", ") ||
+        "Worker processing failed",
+    };
+  }
 
   // TODO (Developer 3):
   // Save telemetry to MongoDB Bucket Pattern
@@ -52,11 +72,6 @@ export const processTelemetry = (data: TelemetryData): ApiResponse => {
     success: true,
     statusCode: 200,
     message: "Telemetry received successfully",
-    data: {
-      vehicleId,
-      latitude,
-      longitude,
-      speed,
-    },
+    data: workerResult.data,
   };
 };
